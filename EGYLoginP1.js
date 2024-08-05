3bfe665d0b2a5da183acd0ab365911dc577569f0
douis ali <mohcine2571976@gmail.com>
2024-08-05 16:55:35 +0100

    Update EGYLoginP1.js

--- EGYLoginP1.js
+++ EGYLoginP1.js
@@ -9,43 +9,32 @@
 // @grant        none
 // ==/UserScript==
 
-(() => {
-    'use strict';
 
-    const credentials = {
-        email: "YASDZ2024@gmail.com", //Write your email here.
-        password: "112345YASDZ@" // Type your account password on bls.
-    };
 
-    const loginUrl = "https://egypt.blsspainglobal.com/Global/Account/Login".toLowerCase();
-    const currentUrl = window.location.href.toLowerCase();
 
-    function fillLoginDetails() {
-        for (let i = 1; i <= 10; i++) {
-            const userId = document.getElementById(`UserId${i}`);
-            const password = document.getElementById(`Password${i}`);
 
-            if (userId && password) {
-                userId.value = credentials.email;
-                password.value = credentials.password;
-                break; // Stop the loop once the fields are found and filled
-            }
-        }
 
-        const btnVerify = document.querySelector("#btnVerify");
-        if (btnVerify) {
-            setTimeout(() => {
-                btnVerify.click();
-            }, 1000); // Delay of 1 second (1000 milliseconds)
-        }
+const email = "yashashos2022@gmail.com"; //Write your email here.
+const passwordValue = "AIS2571976fis@"; // Type your account password on bls.
+
+const loginUrlPrefix = "https://egypt.blsspainglobal.com/Global/Account/Login".toLowerCase();
+
+if (window.location.href.toLowerCase().startsWith(loginUrlPrefix)) {
+  for (let i = 1; i <= 10; i++) {
+    const userId = document.getElementById(`UserId${i}`);
+    const password = document.getElementById(`Password${i}`);
+
+    if (userId && password) {
+      userId.value = email;
+      password.value = passwordValue;
     }
+  }
 
-    if (currentUrl.startsWith(loginUrl)) {
-        const intervalId = setInterval(() => {
-            fillLoginDetails();
-            if (document.querySelector("#UserId1") && document.querySelector("#Password1")) {
-                clearInterval(intervalId); // Stop the interval once elements are found and filled
-            }
-        }, 500); // Check every 500 milliseconds
-    }
-})();
+  const btnVerify = document.querySelector("#btnVerify");
+  if (btnVerify) {
+    setTimeout(() => {
+      btnVerify.click();
+    }, 1000); // مهلة ثانية (1000 ملي ثانية)
+  }
+}
+
